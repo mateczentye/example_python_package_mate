84b4d3eeef8f7848843a7e12680f5a74c77da29c
Mate <mic24@kent.ac.uk>
2021-06-09 12:15:02 +0100

    added version number to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='example_python_package_',
+    name='example_python_package_mate',
     author='Mate',
     author_email='mic24@kent.ac.uk',
     license='MIT',
     url='https://github.com/mateczentye/example_python_package_mate',
     description='magic happens here',
-    packages=["example_pythonpackage_mate"]
+    packages=["example_pythonpackage_mate"],
+    version='0.0.1'
 
 )
