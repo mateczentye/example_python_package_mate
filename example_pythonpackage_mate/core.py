c3c0a6a479e3a59f843e9305ae3a275e89aa8cc7
Mate <mic24@kent.ac.uk>
2021-06-09 12:07:21 +0100

    added package

---
+++ example_pythonpackage_mate/core.py
@@ -0,0 +1,3 @@
+def my_name(firstname):
+    print("My name is {}".format(firstname))
+    return 'my name is {}'.format(firstname)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='example_python_package_',
+    author='Mate',
+    author_email='mic24@kent.ac.uk',
+    license='MIT',
+    url='https://github.com/mateczentye/example_python_package_mate',
+    description='magic happens here',
+    packages=["example_pythonpackage_mate"]
+
+)
