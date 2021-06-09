16f6c6db0aede5df2833d902b3cc76e71a1e7e84
Mate <mic24@kent.ac.uk>
2021-06-09 15:04:05 +0100

    added name test

--- test/test_multi.py
+++ test/test_multi.py
@@ -1,4 +1,4 @@
-from example_python_package_mate import multi
+from example_python_package_mate import multi, my_name
 
 def test_multi_w_small_nums():
     answer = multi(3,3)
@@ -8,3 +8,10 @@ def test_multi_w_big_num():
     answer = multi(100,2)
     assert answer == 200
 
+
+def test_name():
+    input_name = my_name("Mate")
+    correct_name = my_name("Mate")
+    
+    assert input_name == correct_name
+    
