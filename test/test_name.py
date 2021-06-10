9ce632ba3fba0bb7766ec0a32a413b4d84b8e03b
Mate <mic24@kent.ac.uk>
2021-06-10 16:50:24 +0100

    modified test script

--- test/test_name.py
+++ test/test_name.py
@@ -1,7 +1,8 @@
 
 from example_python_package_mate import my_name
 
-def name_test(name_in):
-    name_is = "Mate"
-    assert name_is == name_in
+def test_name():
+    input_name = my_name("Mate")
+    correct_name = my_name("Mate")
     
+    assert input_name == correct_name
