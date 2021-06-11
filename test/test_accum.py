fcf7d2dda4e5acea0404fda427709cf603201989
Mate <mic24@kent.ac.uk>
2021-06-11 14:04:12 +0100

    added markers for tests

--- test/test_accum.py
+++ test/test_accum.py
@@ -7,22 +7,27 @@ from example_python_package_mate.accum import Accumulator
 def accum():
     return Accumulator()
 
+@pytest.mark.accumulator
 def test_accum_init(accum):
     assert accum.count == 0
 
+@pytest.mark.accumulator
 def test_accum_add_one(accum):
     accum.add()
     assert accum.count == 1
 
+@pytest.mark.accumulator
 def test_accum_add_three(accum):
     accum.add(3)
     assert accum.count == 3
 
+@pytest.mark.accumulator
 def test_accum_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
 
+@pytest.mark.accumulator
 def test_accum_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"can't set attribute"):
         accum.count = 10
--- test/test_multi.py
+++ test/test_multi.py
@@ -1,22 +1,27 @@
 import pytest
 from example_python_package_mate import multi, my_name
 
+
+@pytest.mark.multi
 def test_multi_w_small_nums():
     answer = multi(3,3)
     assert answer == 9
 
+@pytest.mark.multi
 def test_multi_w_big_num():
     answer = multi(100,2)
     assert answer == 200
 
-
+@pytest.mark.name
 def test_name():
     input_name = my_name("Mate")
     correct_name = my_name("Mate")
     
     assert input_name == correct_name
 
+
 products = [(2,3,6),(-1,55,-55),(3,-4,-12),(1,100,100)]    
+@pytest.mark.multi
 @pytest.mark.parametrize('a, b, product',products)
 def test_multi_param(a,b,product):
     assert a * b == product
